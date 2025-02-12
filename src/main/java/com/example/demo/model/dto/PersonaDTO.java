f953ab002f22fddd0d9fd7c3706c54fbb4621784
programacion <damharnina25@iesarroyoharnina.es>
2025-02-12 12:12:55 +0100

    issue#02 cambio edad a byte

--- src/main/java/com/example/demo/model/dto/PersonaDTO.java
+++ src/main/java/com/example/demo/model/dto/PersonaDTO.java
@@ -1,4 +1,4 @@
 package com.example.demo.model.dto;
 
-public record PersonaDTO(String nombre, String apellido, int edad,int color) {
+public record PersonaDTO(String nombre, String apellido, byte edad,int color) {
 }
